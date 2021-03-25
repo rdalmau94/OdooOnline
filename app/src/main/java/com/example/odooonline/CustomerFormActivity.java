62e499004076163f58d73004b17deec4fb7ac0d3
Ricard <ricarddalmaucasals@gmail.com>
2021-03-25 16:36:19 +0100

    Afegir nova activity_customer_form.xml y java

---
+++ app/src/main/java/com/example/odooonline/CustomerFormActivity.java
@@ -0,0 +1,14 @@
+package com.example.odooonline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_form);
+    }
+}
